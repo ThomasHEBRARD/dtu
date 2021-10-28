8b693d65f0420a4daff5eab834e2035dbd0e8216
ThomasHEBRARD <thomas.hebrard134@gmail.com>
2021-10-29 00:08:54 +0200

    h

--- 02110/food_truck.py
+++ 02110/food_truck.py
@@ -39,66 +39,74 @@ D = [[10, 7, 5, 3, 0], [1, 0, 2, 16, 15], [0, 1, 1, 11, 12]]
 # for _ in range(M):
 #     D.append(input().split())
 
-total = 0
-C = [0, 1, 3]
-
-# les 2 prochains, ceux qui ont le pire, cad qu'il ne vuat pas le coup de rester
-tab = np.zeros(N)
-
-for i in range(1, 3):
-    for j in range(N):
-        tab[j] += D[i][j]
-
-ind = np.argpartition(tab, -4)[:3]
-print(ind)
-
-# first = np.array(D[0])
-# first_indexes = list(np.argpartition(first, -4)[-4:])
-# first_values = list(first[first_indexes])
-
-# second = np.array(D[1])
-# second_indexes = list(np.argpartition(second, -4)[-4:])
-# second_values = list(second[second_indexes])
-
-# indexes_to_keep = []
-
-# for i in range(len(first_indexes)):
-#     if first_indexes[i] in second_indexes:
-#         indexes_to_keep.append(first_indexes[i])
-
-# print(indexes_to_keep)
-
-# for i in range(M):
-#     total += int(D[0][C[i]])
+initial_C = [0, 1, 3]  # To remove cause i need to test every possible entry
+C = [initial_C]
 
 
-def sol(n, m, d, total):
-    if m == 1:
+def list_exclude(to_exclude):
+    return [i for i in range(N) if i not in to_exclude]
+
+
+def sol(cri, current_total):
+    if cri == M:
+        return current_total
+    if M == 1:
         for _ in range(3):
             c_max = max(d[0])
-            total += c_max
+            current_total += c_max
             for i in range(len(d[0])):
-                if d[0][i] == c_max:
-                    del d[0][i]
+                if D[0][i] == c_max:
+                    del D[0][i]
                     break
-    elif n <= m:
-        return sum([sum(dd) for dd in d])
-    else:
-        for i in range(1, m):
-            min, index = int(d[i][C[0]]), C[0]
+    elif N <= M:
+        return sum([sum(dd) for dd in D])
+    # cc = current_row_index
+    # pi = possible_index
 
-            for j in range(1, m):
-                if int(d[i][C[j]]) < min:
-                    min, index = int(d[i][C[j]]), C[j]
+    d = D[cri]
+    cc = cri - 1
+    case_2_values = [
+        d[pi] if pi in list_exclude([C[cc][0], C[cc][1]]) else None for pi in range(N)
+    ]
+    case_2_max = max(case_2_values)
+    case_2_index = case_2_values.index(case_2_max)
 
-            max_to_change_to = max(
-                [int(d[i][f]) for f in [k for k in range(n) if k not in C]]
-            )
-            c_index_to_change = C.index(index)
+    case_3_values = [
+        d[pi] if pi in list_exclude([C[cc][0], C[cc][2]]) else None for pi in range(N)
+    ]
+    case_3_max = max(case_3_values)
+    case_3_index = case_3_values.index(case_3_max)
 
-            C[c_index_to_change] = d[i].index(max_to_change_to)
-            total += int(d[i][C[0]]) + int(d[i][C[1]]) + int(d[i][C[2]])
-    return total
+    case_4_values = [
+        d[pi] if pi in list_exclude([C[cc][1], C[cc][2]]) else None for pi in range(N)
+    ]
+    case_4_max = max(case_4_values)
+    case_4_index = case_4_values.index(case_4_max)
+
+    case_C = [
+        [C[cc][0], C[cc][1], C[cc][2]],
+        [C[cc][0], C[cc][1], case_2_index],
+        [C[cc][0], case_3_index, C[cc][2]],
+        [case_4_index, C[cc][1], C[cc][2]],
+    ]
+
+    case_1 = current_total + d[C[cc][0]] + d[C[cc][1]] + d[C[cc][2]]
+    case_2 = current_total + d[C[cc][0]] + d[C[cc][1]] + d[case_2_index]
+    case_3 = current_total + d[C[cc][0]] + d[case_3_index] + d[C[cc][2]]
+    case_4 = current_total + d[case_4_index] + d[C[cc][1]] + d[C[cc][2]]
+
+    cases = [case_1, case_2, case_3, case_4]
+
+    current_total = max(cases)
+    max_case_index = cases.index(current_total)
+
+    C.append(case_C[max_case_index])
+    print(C)
+    return sol(cri + 1, max(case_1, case_2, case_3, case_4))
 
 
-print(sol(N, M, D, total))
+# [0, 1, 3]
+# [0, 3, 4]
+# [2, 3, 4]
+
+print(sol(1, 20))
