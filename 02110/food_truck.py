547f5dcc90b422831b78d7e5e6d3f3e672dda66b
ThomasHEBRARD <thomas.hebrard134@gmail.com>
2021-10-30 18:00:55 +0200

    25 points

--- 02110/food_truck.py
+++ 02110/food_truck.py
@@ -32,12 +32,12 @@ D = [[10, 7, 5, 3, 0], [1, 0, 2, 16, 15], [0, 1, 1, 11, 12], [0, 0, 0, 0, 0]]
 import itertools
 from collections import defaultdict
 
-N, M = input().split()
-N = int(N)
-M = int(M)
-D = []
-for _ in range(M):
-    D.append(list(map(int, input().split())))
+# N, M = input().split()
+# N = int(N)
+# M = int(M)
+# D = []
+# for _ in range(M):
+#     D.append(list(map(int, input().split())))
 
 
 def arr_excl(to_exclude):
@@ -55,23 +55,25 @@ def OPT(cri):
     if cri == 0:
         return [[sum([D[0][x] for x in C]), C]]
     else:
-        R = range(N)
         for OPT_DATA in OPT(cri - 1):
             opt_max, optc = OPT_DATA
             c_2_v = [
-                D[cri][pi] if pi in arr_excl([optc[0], optc[1]]) else None for pi in R
+                D[cri][pi] if pi in arr_excl([optc[0], optc[1]]) else None
+                for pi in range(N)
             ]
             case_2_max = max([a for a in c_2_v if a is not None])
             case_2_index = c_2_v.index(case_2_max)
 
             c_3_v = [
-                D[cri][pi] if pi in arr_excl([optc[0], optc[2]]) else None for pi in R
+                D[cri][pi] if pi in arr_excl([optc[0], optc[2]]) else None
+                for pi in range(N)
             ]
             case_3_max = max([a for a in c_3_v if a is not None])
             case_3_index = c_3_v.index(case_3_max)
 
             c_4_v = [
-                D[cri][pi] if pi in arr_excl([optc[1], optc[2]]) else None for pi in R
+                D[cri][pi] if pi in arr_excl([optc[1], optc[2]]) else None
+                for pi in range(N)
             ]
             case_4_max = max([a for a in c_4_v if a is not None])
             case_4_index = c_4_v.index(case_4_max)
@@ -90,12 +92,12 @@ def OPT(cri):
                 opt_max + D[cri][case_4_index] + D[cri][optc[1]] + D[cri][optc[2]],
             ]
 
-            current_total = max(cases)
-            max_case_index = cases.index(current_total)
+            new_total = max(cases)
+            max_case_index = cases.index(new_total)
             le_case_en_question = case_C[max_case_index]
             counter_case_C = [le_case_en_question]
 
-            so = sorted(list_duplicates(D[cri]))
+            so = list_duplicates(D[cri])
             for s in so:
                 ind = list(s)[1]
                 for i in ind:
@@ -109,7 +111,7 @@ def OPT(cri):
                                 for x in le_case_en_question
                             ]
                         )
-            return [[current_total, cas] for cas in counter_case_C]
+            return [[new_total, cas] for cas in counter_case_C]
 
 
 if M == 1:
