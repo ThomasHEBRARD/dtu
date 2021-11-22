4097730f7f46a61d7bcd63d00f85edfcbdfcae58
thebrard <thomas.hebrard134@gmail.com>
2021-11-22 10:52:06 +0100

    dzdz

--- 02110/mand2.py
+++ 02110/mand2.py
@@ -145,17 +145,17 @@ Expected Output:
 # ground_phases = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, -45, -12, -35, -6, 30, 4, -9, -20, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -48, 39, 46, -16, 22, -33, -39, -4, -16, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -28, 10, 2, 49, 27, 18, 22, -21, 6, 49, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 # air_phases = [[1, 252], [1, 252]]
 
-N, G = map(int, input().split())
-TRACK = list(map(int, input().split()))
-nbr_lines = 1 + 2 * G
-air_phases = []
-ground_phases = []
+# N, G = map(int, input().split())
+# TRACK = list(map(int, input().split()))
+# nbr_lines = 1 + 2 * G
+# air_phases = []
+# ground_phases = []
 
-for i in range(3, nbr_lines + 1):
-    if i % 2 == 0:
-        air_phases.append(list(map(int, input().split())))
-    else:
-        ground_phases.append(list(map(int, input().split())))
+# for i in range(3, nbr_lines + 1):
+#     if i % 2 == 0:
+#         air_phases.append(list(map(int, input().split())))
+#     else:
+#         ground_phases.append(list(map(int, input().split())))
 
 
 def computeLPSArray(pat, M, lps):
@@ -177,23 +177,28 @@ def computeLPSArray(pat, M, lps):
                 i += 1
 
 
-def KMPSearch(pat, txt):
+def KMPSearch(pat, txt, NN, need_first=False):
     M = len(pat)
     N = len(txt)
     result = []
     lps = [0] * M
     j = 0
+    R = range(1, NN + 1)
 
     computeLPSArray(pat, M, lps)
 
     i = 0
     while i < N:
+
         if pat[j] == txt[i]:
             i += 1
             j += 1
 
         if j == M:
-            result.append(i - j)
+            if need_first and i - j >= 0:
+                return [i - j]
+            elif i - j in R:
+                result.append(i - j)
             j = lps[j - 1]
 
         elif i < N and pat[j] != txt[i]:
@@ -209,12 +214,8 @@ class Solution:
         self.N = N
         self.result = [0, 0]
         self.first = -1
-        self.mem = {}
 
     def dfs(self, track, air, ground, count):
-        # if len(track) < sum([len(g) for g in ground]) + len(air):
-        #     return
-
         if not ground and not air:
             if (
                 count > self.result[1]
@@ -224,31 +225,21 @@ class Solution:
                 self.result = [self.first + 1, count]
             return
 
-        all_positions = []
-        st = "".join(map(str, ground[0])) + "".join(map(str, track))
-        if st in self.mem:
-            all_positions = self.mem[st]
-        else:
-            all_positions = KMPSearch(ground[0], track)
-            self.mem[st] = all_positions
-        if all_positions:
-            all_positions = [i for i in all_positions if i <= self.N and i >= 1]
+        all_positions = KMPSearch(ground[0], track, self.N, len(ground) != 1)
 
-            for position in all_positions:
-                self.dfs(
-                    track[position + len(ground[0]) :],
-                    air[1:],
-                    ground[1:],
-                    count + position + len(ground[0]),
-                )
+        for position in all_positions:
+            self.dfs(
+                track[position + len(ground[0]) :],
+                air[1:],
+                ground[1:],
+                count + position + len(ground[0]),
+            )
 
     def sol(self, track, air, ground, count):
-        if self.first == -1:
-            all_positions = KMPSearch(ground[0], track)
-            for position in all_positions:
-                self.first = position
-                count = len(ground[0])
-                self.dfs(track[position + len(ground[0]) :], air, ground[1:], count)
+        all_positions = KMPSearch(ground[0], track, self.N, True)
+        self.first = all_positions[0]
+        count = len(ground[0])
+        self.dfs(track[self.first + count :], air, ground[1:], count)
 
         if self.result != [0, 0]:
             return str(self.result[0]) + " " + str(self.result[1])
